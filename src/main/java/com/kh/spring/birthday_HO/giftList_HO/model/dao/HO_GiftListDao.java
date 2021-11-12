ad76ca4f117f49cc3b5b9b1cfc860c8d97654430
JinSeokJang91 <85108311+JinSeokJang91@users.noreply.github.com>
2021-11-12 20:02:19 +0900

    jinseok20211112
    dd

--- src/main/java/com/kh/spring/birthday_HO/giftList_HO/model/dao/HO_GiftListDao.java
+++ src/main/java/com/kh/spring/birthday_HO/giftList_HO/model/dao/HO_GiftListDao.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.spring.birthday_HO.giftList_HO.model.vo.GiftAttachment;
 import com.kh.spring.birthday_HO.giftList_HO.model.vo.HO_GiftList;
 
 @Repository
@@ -18,6 +19,16 @@ public class HO_GiftListDao {
 	public HO_GiftList checkedGiftList(SqlSessionTemplate sqlSession, String giftNo) {
 		// TODO Auto-generated method stub
 		return sqlSession.selectOne("birthdayMapper_HO.checkedGiftList", giftNo);
+	}
+
+	public int insertGift(SqlSessionTemplate sqlSession, HO_GiftList gl) {
+		// TODO Auto-generated method stub
+		return sqlSession.insert("birthdayMapper_HO.insertGift", gl);
+	}
+
+	public int insertAttachment(SqlSessionTemplate sqlSession, GiftAttachment at) {
+		// TODO Auto-generated method stub
+		return sqlSession.insert("birthdayMapper_HO.insertAttachment", at);
 	}
 
 }
--- src/main/java/com/kh/spring/birthday_HO/giftList_HO/model/service/HO_GiftListService.java
+++ src/main/java/com/kh/spring/birthday_HO/giftList_HO/model/service/HO_GiftListService.java
@@ -2,6 +2,7 @@ package com.kh.spring.birthday_HO.giftList_HO.model.service;
 
 import java.util.ArrayList;
 
+import com.kh.spring.birthday_HO.giftList_HO.model.vo.GiftAttachment;
 import com.kh.spring.birthday_HO.giftList_HO.model.vo.HO_GiftList;
 
 public interface HO_GiftListService {
@@ -9,5 +10,9 @@ public interface HO_GiftListService {
 	ArrayList<HO_GiftList> selectGiftList();
 
 	HO_GiftList checkedGiftList(String giftNo);
+
+	void insertGift(HO_GiftList gl);
+
+	void insertAttachment(GiftAttachment at);
 
 }
